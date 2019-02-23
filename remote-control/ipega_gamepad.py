e9192444f7c677416ebda06a80036020615c4869
Giorgos Patsiaouras <giorgospatsiaouras@gmail.com>
2019-02-23 15:10:06 +0100

    Initial Code

---
+++ remote-control/ipega_gamepad.py
@@ -0,0 +1,87 @@
+#import evdev
+from evdev import InputDevice, categorize, ecodes
+import sys, tty, termios, time
+import socket
+import numpy as np
+
+def remap(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+UDP_IP = "192.168.178.242"
+UDP_PORT = 4210
+
+gamepad = InputDevice('/dev/input/event16')
+
+# Constants
+FORWARD = 1;
+REVERSE = 0;
+
+#prints out device info at start
+print(gamepad)
+
+# Save values
+left_x_val = 127
+left_y_val = 127
+right_x_val = 127
+right_y_val = 127
+left_trigger = 0
+right_trigger = 0
+
+for event in gamepad.read_loop():
+
+    if event.type == ecodes.EV_ABS:
+        absevent = categorize(event)
+
+        if ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_X":
+            left_x_val = absevent.event.value
+        elif ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_Y":
+            left_y_val = absevent.event.value
+        elif ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_Z":
+            right_x_val = absevent.event.value
+        elif ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_RZ":
+            right_y_val = absevent.event.value
+        elif ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_BRAKE":
+            left_trigger = absevent.event.value
+        elif ecodes.bytype[absevent.event.type][absevent.event.code] == "ABS_GAS":
+            right_trigger = absevent.event.value
+
+    # Map controllers command to create linear and rotation velocitiesl
+    joy_power = remap(left_y_val, 255, 0, -400, 400)
+    joy_steering = remap(left_x_val, 0, 255, -400, 400)
+
+    camera_yaw = remap(right_x_val, 0, 255, 140, 0)
+    camera_pitch = remap(right_y_val, 0, 255, 180, 0)
+
+    left_motor = joy_power + (joy_steering * 2) / 2
+    right_motor = joy_power + (-joy_steering * 2) / 2
+
+    # print("Left Motor: {0}, Right Motor: {1}, LeftX: {2}, LeftY: {3}".format(left_motor, right_motor, joy_steering, joy_power))
+
+    angular_dir_left = 1
+    angular_dir_right = 1
+
+    if left_motor > 100:
+        angular_dir_left = FORWARD
+    elif left_motor < -100:
+        angular_dir_left = REVERSE
+
+    if right_motor > 100:
+        angular_dir_right = FORWARD
+    elif right_motor < -100:
+        angular_dir_right = REVERSE
+
+    send_list = []
+
+    send_list.append(str(abs(left_motor)))
+    send_list.append(str(abs(right_motor)))
+    send_list.append(str(angular_dir_left))
+    send_list.append(str(angular_dir_right))
+    send_list.append(str(camera_yaw))
+    send_list.append(str(camera_pitch))
+
+    string_to_send = "-".join(send_list)
+
+    print string_to_send
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(string_to_send, (UDP_IP, UDP_PORT))
---
+++ robot/esp8266/main.ino
@@ -0,0 +1,175 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <Servo.h>
+
+// Servo
+Servo camera_pitch;
+Servo camera_yaw;
+
+const int camera_pitch_pin = D6;
+const int camera_yaw_pin = D7;
+
+// WIFI PARAMETERS
+const char* ssid     = "Ziggo9EE38EB";
+const char* password = "ujjx2atrendT";
+
+// UDP SERVER PARAMETERS
+WiFiUDP Udp;
+const unsigned int localUdpPort = 4210;
+const char STATUS_STRING[] = "status";
+char incomingPacket[255];
+
+// MOTOR CONSTANTS AND PARAMETERS
+const int motor_left_pwm = 5;
+const int motor_right_pwm = 4;
+const int motor_left_dir = 0;
+const int motor_right_dir = 2;
+const int motor_left_forward = LOW;
+const int motor_right_forward = HIGH;
+const int motor_left_reverse = HIGH;
+const int motor_right_reverse = LOW;
+
+void setup_pins() {
+  // Motor pins
+  pinMode(motor_left_pwm, OUTPUT);
+  pinMode(motor_left_dir, OUTPUT);
+  pinMode(motor_right_pwm, OUTPUT);
+  pinMode(motor_right_dir, OUTPUT);
+}
+
+void setup_wifi() {
+  // Connect the ESP8266 the the wifi AP
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void test_motors() {
+  // Set pwm in 1/4 of full power
+  analogWrite(motor_left_pwm, 256);
+  analogWrite(motor_right_pwm, 256);
+
+  // Go Forwards 
+  digitalWrite(motor_left_dir, motor_left_forward);
+  digitalWrite(motor_right_dir, motor_right_forward);
+  delay(2000);
+
+  // Go Reverse
+  digitalWrite(motor_left_dir, motor_left_reverse);
+  digitalWrite(motor_right_dir, motor_right_reverse);
+  delay(2000);
+
+  // Shutdown pwm
+  analogWrite(motor_left_pwm, 0);
+  analogWrite(motor_right_pwm, 0);
+}
+
+void test_servos() {
+  int pos;
+
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    camera_pitch.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    camera_pitch.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    camera_yaw.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    camera_yaw.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+void drive_motors(int re_left_motor_pwm, int re_right_motor_pwm, int re_left_motor_dir, int re_right_motor_dir) {
+  if (re_left_motor_dir == 1) {
+    digitalWrite(motor_left_dir, motor_left_forward);  
+  } else {
+    digitalWrite(motor_left_dir, motor_left_reverse);
+  }
+  if (re_right_motor_dir == 1) {
+    digitalWrite(motor_right_dir, motor_right_forward);   
+  } else {
+    digitalWrite(motor_right_dir, motor_right_reverse);   
+  }
+  
+  analogWrite(motor_left_pwm, re_left_motor_pwm);
+  analogWrite(motor_right_pwm, re_right_motor_pwm);
+}
+
+void drive_servos(int re_camera_yaw, int re_camera_pitch) {
+  camera_yaw.write(re_camera_yaw);
+  camera_pitch.write(re_camera_pitch);
+}
+
+void setup() {
+  // Open Serial port
+  Serial.begin(115200);
+
+  setup_pins();
+
+  // Servo
+  camera_pitch.attach(camera_pitch_pin);
+  camera_yaw.attach(camera_yaw_pin);
+  
+  setup_wifi();
+
+  Udp.begin(localUdpPort);
+}
+
+void loop() {
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+    int len = Udp.read(incomingPacket, 255);
+    if (len > 0)
+    {
+      incomingPacket[len] = 0;
+    }
+    Serial.printf("UDP packet contents: %s\n", incomingPacket);
+    
+    if (incomingPacket[0] != STATUS_STRING[0]) {
+      char *token = strtok(incomingPacket, "-"); // tokenize the string using colons as the delimiters
+  
+      int re_left_motor_pwm = atoi(token);
+      token = strtok(NULL, "-");
+      int re_right_motor_pwm = atoi(token);
+      token = strtok(NULL, "-");
+      int re_left_motor_dir = atoi(token);
+      token = strtok(NULL, "-");
+      int re_right_motor_dir = atoi(token);
+      token = strtok(NULL, "-");
+      int re_camera_yaw = atoi(token);
+      token = strtok(NULL, "-");
+      int re_camera_pitch = atoi(token);
+      token = strtok(NULL, "-");
+  
+      drive_motors(re_left_motor_pwm, re_right_motor_pwm, re_left_motor_dir, re_right_motor_dir);
+      drive_servos(re_camera_yaw, re_camera_pitch)
+    } else {
+      Udp.beginPacket(Udp.remoteIP(), localUdpPort);
+      String str = String(analogRead(motor_left_pwm))+"-"+String(analogRead(motor_right_pwm));
+      char replyPacket[str.length()+1];
+      strcpy(replyPacket, str.c_str());
+      
+      Serial.println(replyPacket);
+      Udp.write(replyPacket);
+      Udp.endPacket();
+    }
+  }
+  delay(10);
+}
